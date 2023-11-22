7fd18a9de5c7a63584f476c88b2d0849a955f314
elias.oliveira <elias.oliveira@adviseu.com.br>
2023-11-22 15:52:23 -0300

    initial commit

---
+++ src/main/java/com/selfmanager/personalapiselfmanager/PersonalApiSelfmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.selfmanager.personalapiselfmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalApiSelfmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalApiSelfmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfmanager/personalapiselfmanager/PersonalApiSelfmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfmanager.personalapiselfmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApiSelfmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
